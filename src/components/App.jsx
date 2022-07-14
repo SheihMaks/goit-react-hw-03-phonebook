1921a93e7f722b96cd5ef516826fede53eaf74c3
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-07-15 00:38:44 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,23 @@ export class App extends React.Component {
   filterId=nanoid();
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter:'',
+  }
+
+  componentDidMount(){
+  const contacts=JSON.parse(localStorage.getItem('contacts'));
+    if (contacts){
+    this.setState({contacts})}
+  }
+
+  componentDidUpdate(prevState){
+    if(this.state.contacts !== prevState.contacts){
+    localStorage.setItem("contacts",JSON.stringify(this.state.contacts))}
   }
 
 formHandleSubmit=(data) => {
@@ -25,7 +36,8 @@ formHandleSubmit=(data) => {
 }
   if (this.state.contacts.find(el=>el.name===data.name)){
 window.alert(`${data.name} is already in contacts`) } 
-else{ this.setState((prevState)=>({contacts:[data,...prevState.contacts]}))}}
+else{ this.setState((prevState)=>({contacts:[data,...prevState.contacts]}))
+ }}
 
 
 onUserFilter=(e)=>{
